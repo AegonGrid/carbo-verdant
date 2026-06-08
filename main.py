67f564241f75e4d5813d035aa1a098efb5fea044
Aegon <aegongrid@gmail.com>
2026-06-08 12:56:47 +0200

    Initialize main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from carbo-verdant!")
+import geopandas as gpd
 
+def main():
+
+    aoi = gpd.read_file("data/example_polygon_2.geojson")  
+    print(aoi.name)
 
 if __name__ == "__main__":
     main()
