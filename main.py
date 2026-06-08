e89c7cc75c2bb1f108129b99d857b9f41e1d591a
Aegon <aegongrid@gmail.com>
2026-06-08 18:22:00 +0200

    WIP download dummy s2 data

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import geopandas as gpd
+from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
+from dotenv import load_dotenv
+import os
 
 def main():
 
-    aoi = gpd.read_file("data/example_polygon_2.geojson")  
-    print(aoi.name)
+    load_dotenv()
+    COPERNICUS_USERNAME = os.getenv('COPERNICUS_USERNAME')
+    COPERNICUS_PASSWORD = os.getenv('COPERNICUS_PASSWORD')
 
+    aoi_path = "data/example_polygon_2.geojson"
+    
+    api = SentinelAPI(COPERNICUS_USERNAME, COPERNICUS_PASSWORD, 'https://apihub.copernicus.eu/apihub')
+    footprint = geojson_to_wkt(read_geojson(aoi_path))
+    products = api.query(
+        area=footprint,
+        date=("20200101", "20201202"),  # YYYYMMDD format
+        platformname="Sentinel-2",
+        producttype="S2MSI2A",  # Level-2A (surface reflectance)
+        cloudcoverpercentage=(0, 10)  # 0-10% cloud cover
+    )
+    api.download_all(products[:1], directory="data/sentinel2")
+    
 if __name__ == "__main__":
     main()
